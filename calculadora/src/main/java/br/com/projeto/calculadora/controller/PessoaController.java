de452cce54e4f3a7eeb9fab41257e219d4556fb0
CauaConceicao20 <CauaConceicaoDev@hotmail.com>
2024-04-24 10:57:18 -0300

    feat: adiciona endpoint create no PessoaController

--- calculadora/src/main/java/br/com/projeto/calculadora/controller/PessoaController.java
+++ calculadora/src/main/java/br/com/projeto/calculadora/controller/PessoaController.java
@@ -1,9 +1,25 @@
 package br.com.projeto.calculadora.controller;
 
+import br.com.projeto.calculadora.dtos.DadosPessoa;
+import br.com.projeto.calculadora.service.PessoaService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/pessoa")
 public class PessoaController {
+
+    @Autowired
+    PessoaService pessoaService;
+
+    @PostMapping("/create")
+    @Transactional
+    public void adicionaPessoa(@RequestBody @Valid DadosPessoa dadosPessoa) {
+        pessoaService.save(dadosPessoa);
+    }
 }
