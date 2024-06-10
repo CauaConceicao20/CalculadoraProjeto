8ba0ab131f55d62811a248b997d926e9e4943693
CauaConceicao20 <CauaConceicaoDev@hotmail.com>
2024-06-10 10:37:21 -0300

    ci: altera configuraçÃão sonarCloud

--- calculadora/src/test/java/br/com/projeto/calculadora/service/OperacoesServiceTest.java
+++ calculadora/src/test/java/br/com/projeto/calculadora/service/OperacoesServiceTest.java
@@ -10,14 +10,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
+
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
+
 @ExtendWith(MockitoExtension.class)
 public class OperacoesServiceTest {
 
