7a92b1a5918312b2249f6a17d14dc8693f7a8579
CauaConceicao20 <CauaConceicaoDev@hotmail.com>
2024-06-11 17:19:41 -0300

    refactor: retira imports desnecessarios

--- calculadora/src/main/java/br/com/projeto/calculadora/controller/PessoaController.java
+++ calculadora/src/main/java/br/com/projeto/calculadora/controller/PessoaController.java
@@ -1,12 +1,5 @@
 package br.com.projeto.calculadora.controller;
 
-import br.com.projeto.calculadora.dtos.DadosPessoa;
-import br.com.projeto.calculadora.service.PessoaService;
-import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
